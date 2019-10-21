3fc914ff2b8af5f2e6c7ead430942bb9a7f822e8
MartinCayuelas <martincayuelas@gmail.com>
2019-10-21 21:27:53 +0200

    [FIX] name

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -10,7 +10,6 @@ object SimpleApp {
 
   def main(args: Array[String]) {
     val logFile = "/home/martin/Bureau/test.md" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
