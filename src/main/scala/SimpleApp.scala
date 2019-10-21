b0bb16ae39ecbce66a94beafdb48e4a096f4ecbc
MartinCayuelas <martincayuelas@gmail.com>
2019-10-21 21:14:46 +0200

    [INIT]Test spark with counting lines with a and b

---
+++ src/main/scala/SimpleApp.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.sql.SparkSession
+
+object SimpleApp {
+  val spark = SparkSession
+    .builder()
+    .appName("Click prediction")
+    .config("spark.master", "local")
+    .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+
+  def main(args: Array[String]) {
+    val logFile = "/home/martin/Bureau/test.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
