b31d4893e087c9e61a18ec5747693fa777ab8d45
Nathan Guillaud <nathan.guillaud@orange.fr>
2019-10-22 12:59:01 +0200

    Naive Bayes on test data

---
+++ src/main/scala/prediction/NaiveBayes.scala
@@ -0,0 +1,45 @@
+package prediction
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.sql.SparkSession
+
+object NaiveBayesPrediction {
+
+  val spark: SparkSession = SparkSession
+    .builder()
+    .appName("Click prediction")
+    .config("spark.master", "local")
+    .getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+
+  def createModel(filePath: String): NaiveBayesModel = {
+    // Load and parse the data file.
+    val data = MLUtils.loadLibSVMFile(spark.sparkContext, filePath)
+
+    // Split data into training (60%) and test (40%).
+    val Array(training, test) = data.randomSplit(Array(0.7, 0.3))
+
+    val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
+
+    val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
+    val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+
+    println("ACCURACY : " + accuracy)
+
+    model
+  }
+
+  def saveModel(model: NaiveBayesModel): Unit = {
+    // Save and load model
+    model.save(spark.sparkContext, "target/tmp/myNaiveBayesModel")
+  }
+
+  def loadModel(modelPath: String): NaiveBayesModel = {
+    NaiveBayesModel.load(spark.sparkContext, modelPath)
+  }
+
+  def main(args: Array[String]): Unit = {
+    createModel("data/fake_data.txt")
+  }
+
+}
