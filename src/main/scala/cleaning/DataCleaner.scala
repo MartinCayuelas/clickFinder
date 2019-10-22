ebd9337448f6f236d88ca821cfcc49ca4c958854
Nicolas Guary <nicolas.guary@etu.umontpellier.fr>
2019-10-22 12:28:46 +0200

    cleaner

--- src/main/scala/cleaning/DataCleaner.scala
+++ src/main/scala/cleaning/DataCleaner.scala
@@ -3,6 +3,8 @@ package cleaning
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
+import scala.annotation.tailrec
+
 object DataCleaner {
 
   val spark: SparkSession = SparkSession
@@ -21,33 +23,65 @@ object DataCleaner {
   }
 
   def selectData(dataFrame: DataFrame): DataFrame = {
-    val columns =  Seq("appOrSite", "bidFloor", "interests", "media", "publisher", "user", "size", "type")
-    dataFrame.select( columns.head, columns.tail: _*)
+    val columns =  Seq("appOrSite", "bidFloor","media", "publisher", "os", "interests",  "user", "size", "type")
+    dataFrame.select(columns.head, columns.tail: _*)
   }
 
 
   def cleanOS(dataFrame: DataFrame) : DataFrame = {
-    dataFrame.na.fill("UNKNOWN",Seq("os"))
-    dataFrame.withColumn("os", when(col("os") === "ios", "iOS")
-      .otherwise(when(col("os") === "Android", "android"))
-      .otherwise(when(col("os") === "Windows Phone OS", "WindowsPhone"))
-      .otherwise(col("make"))
+    val df_non_null = dataFrame.na.fill("UNKNOWN",Seq("os"))
+    df_non_null.withColumn("os", when(col("os") === "ios", "iOS")
+      .otherwise(when(col("os") === "Android", "android")
+        .otherwise(when(col("os") === "Unknown", "UNKNOWN")
+          .otherwise(when(col("os") === "other", "UNKNOWN")
+            .otherwise(when(col("os") === "Windows Mobile OS", "WindowsPhone")
+              .otherwise(when(col("os") ===  "WindowsMobile", "WindowsPhone")
+                .otherwise(when(col("os") ===  "windows", "WindowsPhone")
+                  .otherwise(when(col("os") === "Windows Phone OS"  , "WindowsPhone")
+                    .otherwise(col("os")))))))))
     )
   }
 
-  def clean(dataFrame: DataFrame): DataFrame = {
-    cleanOS(dataFrame)
+  def cleanBidFloor(dataFrame: DataFrame) : DataFrame ={
+    dataFrame.na.fill(0,Seq("bidFloor"))
   }
 
-  def retrieveDataFrame(dataFrame: DataFrame): DataFrame = {
+  /**
+   * Applies all the cleaning method to the dataframe in parameter
+   * @param dataFrame
+   * @return
+   */
+  def clean(dataFrame: DataFrame): DataFrame = {
+    val methods: Seq[DataFrame => DataFrame] =  Seq(cleanOS, cleanBidFloor)
+    @tailrec
+    def applyMethods(methods: Seq[DataFrame => DataFrame], res: DataFrame): DataFrame = {
+      methods match {
+        case Seq() => res
+        case _ => applyMethods(methods.tail, methods.head(res))
+      }
+    }
+    applyMethods(methods, dataFrame)
+  }
+
+  /**
+   * TODO - Pass an array of string corresponding to the columns to select
+   * @return The dataFrame with clean entries
+   */
+  def retrieveDataFrame(): DataFrame = {
     val df = selectData(readDataFrame())
     clean(df)
   }
 
+
+  /**
+   * TEST CASE
+   */
   def main(args: Array[String]) {
     val df = selectData(readDataFrame())
     val res = clean(df)
     res.printSchema
+    res.select("os").distinct.show()
+    res.select("bidFloor").distinct.show()
     spark.stop()
   }
 }
