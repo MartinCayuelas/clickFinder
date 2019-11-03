2f2d6d68a57d86cb19a6bbc48fb46e0341ac6aac
Nicolas Guary <nicolas.guary@etu.umontpellier.fr>
2019-11-03 18:09:41 +0100

    new split strategy

--- src/main/scala/prediction/Predicter.scala
+++ src/main/scala/prediction/Predicter.scala
@@ -1,8 +1,10 @@
+import cleaning.DataAnalysis.spark
 import cleaning.DataCleaner
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.classification.{LogisticRegression, RandomForestClassifier}
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.sql.functions.{col, when}
 
 
 object Predicter {
@@ -17,13 +19,24 @@ object Predicter {
 
     val raw_data = DataCleaner.retrieveDataFrame()
 
+    val raw_data_pos = raw_data.filter(col("label") === 1)
+    val raw_data_neg = raw_data.filter(col("label") === 0)
+
     val featuresCols = Array("appOrSite", "timestamp", "size", "os", "bidFloor", "type")
     val assembler = new VectorAssembler()
       .setInputCols(featuresCols)
       .setOutputCol("features")
-    val data = assembler.transform(raw_data).select( $"features", $"label")
 
-    val Array(trainingData, testData) = data.randomSplit(Array(0.8, 0.2))
+    val data_pos = assembler.transform(raw_data_pos).select( $"features", $"label")
+    val data_neg = assembler.transform(raw_data_neg).select( $"features", $"label")
+
+    val Array(trainingDataPos, testDataPos) = data_pos.randomSplit(Array(0.8, 0.2))
+    val Array(trainingDataNeg, testDataNeg) = data_neg.randomSplit(Array(0.8, 0.2))
+
+    val trainingData = trainingDataPos.union(trainingDataNeg)
+    val testData = testDataPos.union(testDataNeg)
+
+    //TODO - Algorithms predict only 200k lines?
 
     val logisticRegression = new LogisticRegression()
       .setMaxIter(10)
