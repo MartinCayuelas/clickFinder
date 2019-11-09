90e5da64b3ecf8f077649c9137b2f29d96277062
MartinCayuelas <martincayuelas@gmail.com>
2019-11-09 17:11:39 +0100

    [Add] Launcher

---
+++ src/main/scala/Launcher.scala
@@ -0,0 +1,3 @@
+object Launcher {
+
+}
---
+++ src/main/scala/predicting/Predictor.scala
@@ -0,0 +1,10 @@
+package predicting
+
+import org.apache.spark.sql.SparkSession
+
+object Predicter {
+
+  def predict(spark: SparkSession, str: String) = ???
+
+
+}
