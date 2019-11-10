e7618c1393a5a23e0ffec3d6dbe56860ad7314d1
MartinCayuelas <martincayuelas@gmail.com>
2019-11-10 21:27:35 +0100

    [Done] Main for trainning

--- src/main/scala/train/logisticregression/Trainer.scala
+++ src/main/scala/train/logisticregression/Trainer.scala
@@ -9,24 +9,25 @@ import utils.Tools
 
 object Trainer {
 
-  def main(args: Array[String]) {
+  def train(spark: SparkSession, pathToFile: String) {
 
-    val spark = SparkSession
-      .builder()
-      .appName("ClickFinder")
-      .config("spark.master", "local")
-      .getOrCreate()
+    /* val spark = SparkSession
+       .builder()
+       .appName("ClickFinder")
+       .config("spark.master", "local")
+       .getOrCreate()
+ */
     import spark.implicits._
 
     //val raw_data = Tools.retrieveDataFrameCleaned()
-    val raw_data = Tools.limitDataFrame(Tools.retrieveDataFrameCleaned(), 100000)
+    val raw_data = Tools.limitDataFrame(Tools.retrieveDataFrameCleaned(pathToFile), 100000)
 
     val featuresCols = Array("appOrSite", "size", "os", "bidFloor", "type", "exchange", "media", "IAB1", "IAB2", "IAB3", "IAB4", "IAB5", "IAB6", "IAB7", "IAB8", "IAB9", "IAB10", "IAB11", "IAB12", "IAB13", "IAB14", "IAB15", "IAB16", "IAB17", "IAB18", "IAB19", "IAB20", "IAB21", "IAB22", "IAB23", "IAB24", "IAB25", "IAB26")
     val assembler = new VectorAssembler()
       .setInputCols(featuresCols)
       .setOutputCol("features")
 
-    val data = assembler.transform(raw_data).select( $"features", $"label")
+    val data = assembler.transform(raw_data).select($"features", $"label")
     val Array(trainingData, testData) = data.randomSplit(Array(0.8, 0.2), seed = 42)
 
     val balanced_dataset = Tools.balanceDataset(trainingData)
