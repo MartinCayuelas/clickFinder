5c0e0d0896acc1424df927842c1a4d94d51859e5
MartinCayuelas <martincayuelas@gmail.com>
2019-11-10 20:28:42 +0100

    [FIX] pathFile

--- src/main/scala/predict/Predictor.scala
+++ src/main/scala/predict/Predictor.scala
@@ -19,7 +19,7 @@ object Predictor {
     val df = data.drop("label").withColumn("id", monotonically_increasing_id())
 
 
-    val dataAssembled = Tools.retrieveDataFrameCleaned(data)
+    val dataAssembled = Tools.retrieveDataFrameCleaned(dataPath)
     /*
     val featuresCols = Array("appOrSite", "size", "os", "bidFloor", "type", "exchange", "media", "IAB1", "IAB2", "IAB3", "IAB4", "IAB5", "IAB6", "IAB7", "IAB8", "IAB9", "IAB10", "IAB11", "IAB12", "IAB13", "IAB14", "IAB15", "IAB16", "IAB17", "IAB18", "IAB19", "IAB20", "IAB21", "IAB22", "IAB23", "IAB24", "IAB25", "IAB26")
 
