0a1c917bd2e81c151b230da713f140e1d4c73e71
Nathan Guillaud <nathan.guillaud@orange.fr>
2019-11-10 20:36:49 +0100

    RandomForest without cross validator

--- src/main/scala/train/LogisticRegressionBenchMark.scala
+++ src/main/scala/train/LogisticRegressionBenchMark.scala
@@ -3,11 +3,9 @@ package train
 import java.io.{BufferedWriter, File, FileWriter}
 
 import eval.Evaluator
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.{LogisticRegression, RandomForestClassifier}
+import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import utils.Tools
@@ -15,77 +13,9 @@ import utils.Tools
 
 object LogisticRegressionBenchMark {
 
-  /*def logisticRegression(balanced_dataset: DataFrame, testData: DataFrame, maxIter: Int, regParam: Double, threshold: Double) = {
+  def logisticRegression(balanced_dataset: DataFrame, testData: DataFrame, maxIter: Int, regParam: Double, threshold: Double) = {
 
     Tools.writeFile(s"--------------------------------------------------------------------\nPARAMS:\n maxIter: $maxIter \n regParam $regParam \n threshold $threshold", "results/resultLR.txt")
-
-    val randomForestClassifier = new RandomForestClassifier()
-      .setImpurity("gini")
-      .setMaxDepth(3)
-      .setNumTrees(20)
-      .setFeatureSubsetStrategy("auto")
-      .setSeed(42)
-
-    // evaluate model with area under ROC
-    val evaluator = new BinaryClassificationEvaluator()
-      .setLabelCol("label")
-      .setMetricName("areaUnderROC")
-
-
-    val stages = Array(assembler, randomForestClassifier)
-
-    // build pipeline
-    val pipeline = new Pipeline().setStages(stages)
-    val pipelineModel = pipeline.fit(trainingData)
-
-    // test model with test data
-    val pipelinePredictionDf = pipelineModel.transform(testData)
-    pipelinePredictionDf.show(10)
-    val pipelineAccuracy = evaluator.evaluate(pipelinePredictionDf)
-    println(pipelineAccuracy)
-
-    val paramGrid = new ParamGridBuilder()
-      /*
-  .addGrid(randomForestClassifier.maxBins, Array(100, 90, 80))
-  .addGrid(randomForestClassifier.numTrees, Array(40, 35, 30))
-  .addGrid(randomForestClassifier.maxDepth, Array(7, 8, 9))
-  .addGrid(randomForestClassifier.impurity, Array("entropy", "gini"))
-*/
-      .addGrid(randomForestClassifier.maxBins, Array(31))
-      .addGrid(randomForestClassifier.maxDepth, Array(8))
-      .addGrid(randomForestClassifier.impurity, Array("gini"))
-
-      .build()
-
-    // define cross validation stage to search through the parameters
-    // K-Fold cross validation with BinaryClassificationEvaluator
-    val cv = new CrossValidator()
-      .setEstimator(pipeline)
-      .setEvaluator(evaluator)
-      .setEstimatorParamMaps(paramGrid)
-      .setNumFolds(5)
-
-    // fit will run cross validation and choose the best set of parameters
-    // this will take some time to run
-    val cvModel = cv.fit(trainingData)
-
-    // test cross validated model with test data
-    val cvPredictionDf = cvModel.transform(testData)
-    println("evaluation:")
-    println(evaluator.evaluate(cvPredictionDf))
-    cvPredictionDf.show(10)
-
-    Evaluator.retrieveMetrics(cvPredictionDf, "results/resultRF.txt")
-
-    cvModel.write.overwrite().save("model/randomForestModel")
-    Tools.saveDataFrameToCsv(cvPredictionDf.select($"label", $"prediction"), "RANDOMF")
-
-
-
-
-
-
-
 
     val lr = new LogisticRegression()
       .setLabelCol("label")
@@ -119,6 +49,6 @@ object LogisticRegressionBenchMark {
     Tools.writeFile("areaUnderROC: " + accuracyBLR +"\n", "results/resultLR.txt")
     //Tools.saveDataFrameToCsv(predictionsBalancedLR.select($"label", $"prediction"), "predictionBLR")
 
-  }*/
+  }
 
 }
--- src/main/scala/train/RandomForest.scala
+++ src/main/scala/train/RandomForest.scala
@@ -21,93 +21,46 @@ object RandomForest {
       .getOrCreate()
     import spark.implicits._
 
-    val data = Tools.retrieveDataFrameCleaned()
-    val raw_data = Tools.limitDataFrame(data, 100000)
+    val raw = Tools.retrieveDataFrameCleaned()
+    val raw_data = Tools.limitDataFrame(raw, 1000)
 
 
-    val featuresCols = Array("appOrSite", "timestamp", "size", "os", "bidFloor", "type", "exchange", "media", "IAB1", "IAB2", "IAB3", "IAB4", "IAB5", "IAB6", "IAB7", "IAB8", "IAB9", "IAB10", "IAB11", "IAB12", "IAB13", "IAB14", "IAB15", "IAB16", "IAB17", "IAB18", "IAB19", "IAB20", "IAB21", "IAB22", "IAB23", "IAB24", "IAB25", "IAB26")
+    val featuresCols = Array("appOrSite", "size", "os", "bidFloor", "type", "exchange", "media", "IAB1", "IAB2", "IAB3", "IAB4", "IAB5", "IAB6", "IAB7", "IAB8", "IAB9", "IAB10", "IAB11", "IAB12", "IAB13", "IAB14", "IAB15", "IAB16", "IAB17", "IAB18", "IAB19", "IAB20", "IAB21", "IAB22", "IAB23", "IAB24", "IAB25", "IAB26")
     val assembler = new VectorAssembler()
       .setInputCols(featuresCols)
       .setOutputCol("features")
 
-    //val data = assembler.transform(raw_data)
-    //val balanced_dataset = balanceDataset(data)
-
-    val Array(trainingData, testData) = raw_data.randomSplit(Array(0.8, 0.2), seed = 42)
+    val data = assembler.transform(raw_data).select( $"features", $"label")
+    val Array(trainingData, testData) = data.randomSplit(Array(0.8, 0.2), seed = 42)
 
     val randomForestClassifier = new RandomForestClassifier()
       .setImpurity("gini")
       .setMaxDepth(3)
       .setNumTrees(20)
+      .setMaxBins(31)
       .setFeatureSubsetStrategy("auto")
       .setSeed(42)
 
-    //val randomForestModel = randomForestClassifier.fit(trainingData)
+    val balancedRF = randomForestClassifier.fit(trainingData)
+    balancedRF.write.overwrite().save("models/balancedModel")
 
-    //val predictionDf = randomForestModel.transform(testData)
+    val predictionsBalancedRF = balancedRF.transform(testData)
 
-
-    // evaluate model with area under ROC
+    //Evaluator for the classification
     val evaluator = new BinaryClassificationEvaluator()
       .setLabelCol("label")
+      .setRawPredictionCol("prediction")
       .setMetricName("areaUnderROC")
 
-    // measure the accuracy
-   // val accuracy = evaluator.evaluate(predictionDf)
-   // println(accuracy)
 
+    val accuracyBRF = evaluator.evaluate(predictionsBalancedRF)
+    println("areaUnderROC: " + accuracyBRF)
 
+    predictionsBalancedRF.show(50)
 
+    Evaluator.retrieveMetrics(predictionsBalancedRF, "results/resultRF.txt")
 
-    // VectorAssembler and StringIndexer are transformers
-    // LogisticRegression is the estimator
-    val stages = Array(assembler, randomForestClassifier)
-
-    // build pipeline
-    val pipeline = new Pipeline().setStages(stages)
-    val pipelineModel = pipeline.fit(trainingData)
-
-    // test model with test data
-    val pipelinePredictionDf = pipelineModel.transform(testData)
-    pipelinePredictionDf.show(10)
-    val pipelineAccuracy = evaluator.evaluate(pipelinePredictionDf)
-    println(pipelineAccuracy)
-
-    val paramGrid = new ParamGridBuilder()
-      /*
-  .addGrid(randomForestClassifier.maxBins, Array(100, 90, 80))
-  .addGrid(randomForestClassifier.numTrees, Array(40, 35, 30))
-  .addGrid(randomForestClassifier.maxDepth, Array(7, 8, 9))
-  .addGrid(randomForestClassifier.impurity, Array("entropy", "gini"))
-*/
-  .addGrid(randomForestClassifier.maxBins, Array(31))
-  .addGrid(randomForestClassifier.maxDepth, Array(8))
-  .addGrid(randomForestClassifier.impurity, Array("gini"))
-
-      .build()
-
-    // define cross validation stage to search through the parameters
-    // K-Fold cross validation with BinaryClassificationEvaluator
-    val cv = new CrossValidator()
-      .setEstimator(pipeline)
-      .setEvaluator(evaluator)
-      .setEstimatorParamMaps(paramGrid)
-      .setNumFolds(5)
-
-    // fit will run cross validation and choose the best set of parameters
-    // this will take some time to run
-    val cvModel = cv.fit(trainingData)
-
-    // test cross validated model with test data
-    val cvPredictionDf = cvModel.transform(testData)
-    println("evaluation:")
-    println(evaluator.evaluate(cvPredictionDf))
-    cvPredictionDf.show(10)
-
-    Evaluator.retrieveMetrics(cvPredictionDf, "results/resultRF.txt")
-
-    cvModel.write.overwrite().save("model/randomForestModel")
-    Tools.saveDataFrameToCsv(cvPredictionDf.select($"label", $"prediction"), "RANDOMF")
+    Tools.saveDataFrameToCsv(predictionsBalancedRF.select($"label", $"prediction"), "predictionBRF")
 
     spark.stop()
   }
