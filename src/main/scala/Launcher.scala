42a9021446577fa33c69531bfdbefbcdff64723d
MartinCayuelas <martincayuelas@gmail.com>
2019-11-11 16:42:50 +0100

    [ADD] test for 1000 rows

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -13,6 +13,7 @@ object Launcher extends App {
 
     if(args.length == 0) println("You should provide an action and a path to your dataset.")
     else if(args(0) == "predict")  Predictor.predict(spark,args(1))
+    else if(args(0) == "predict1000")  Predictor.predict1000(spark,args(1))
     else if(args(0) == "train")  Trainer.train(spark,args(1))
 
     spark.close()
